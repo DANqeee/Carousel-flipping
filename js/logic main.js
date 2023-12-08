2dced1e6d8f7cb7824bee96f7a7ded317ae57404
DAN <mozhayen@bk.ru>
2023-12-08 21:50:09 +0300

    Carousel

---
+++ js/logic main.js
@@ -0,0 +1,29 @@
+var Img = [
+    "../image/img.png",
+    "../image/img_2.png",
+    "../image/img_3.png",
+    "../image/img_4.png",
+    "../image/img_5.png"
+];
+var num = 0;
+function  FlippingThrough(sign){
+if (sign === "+" && num < 4){
+    let bool = 1;
+
+while (bool ===1 ){
+    num++;
+    document.getElementById("mainPNG").src=Img[num];
+    console.log(Img.length,num);
+    bool = 2;
+}
+}
+    else if (sign === "-" && num>0){
+    let bool = 1;
+    while (bool ===1){
+        num--;
+        document.getElementById("mainPNG").src=Img[num];
+        console.log("----");
+        bool = 2;
+    }
+    }
+}
